fbe3a35d4fd0027035999c3551f36949e11068c7
Maël Naccache Tüfekçi <contact@maeln.com>
2020-10-01 10:02:57 +0200

    Change bg color.

--- photo.js
+++ photo.js
@@ -5,91 +5,103 @@ const pac = new Path2D("m-31 114a27 29 0 0 1-32-11 27 29 0 0 1 4.3-36 27 29 0 0 
 
 const canvas = document.getElementById('mcv')
 const ctx = canvas.getContext('2d');
-ctx.save()
-
 const { width, height } = canvas.getBoundingClientRect();
 
-// background
-ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, width, height)
-
-
-// the circle
-const circleSpacing = 5
-var circle = new Path2D()
-circle.arc(width / 2, height / 2, width / 2 - circleSpacing, 0, 2 * Math.PI)
-
-ctx.lineWidth = 2;
-ctx.strokeStyle = 'white'
-ctx.stroke(circle)
-
-const rectWidth = 5
-const rectHeight = 10
-const clockLine = (i, m) => {
+const drawPattern = (backgroundColor) => {
     ctx.save()
-    ctx.fillStyle = `hsl(${Math.floor(i / m * 360)},100%,50%)`
-    ctx.translate(-rectWidth / 2, -rectHeight / 2)
-    ctx.fillRect(0, 0, rectWidth, rectHeight)
-    ctx.restore()
-}
+    // background
+    ctx.fillStyle = backgroundColor
+    ctx.fillRect(0, 0, width, height)
 
-ctx.translate(width / 2, height / 2) // move origin to center
-const step = 60
-for (let d = 0; d < step; ++d) {
-    ctx.save()
-    const rad = 2 * Math.PI / step * d
-    ctx.rotate(rad)
-    ctx.translate(0, -height / 2 + circleSpacing)
-    clockLine(d, step)
-    ctx.restore()
-}
+    // the circle
+    const circleSpacing = 5
+    var circle = new Path2D()
+    circle.arc(width / 2, height / 2, width / 2 - circleSpacing, 0, 2 * Math.PI)
 
-const figWidth = 15
-const figHeight = 15
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'white'
+    ctx.stroke(circle)
 
-const drawCircle = () => {
-    const c = new Path2D()
-    c.arc(0, 0, figWidth / 2, 0, 2 * Math.PI)
-    ctx.stroke(c)
-    ctx.fill(c)
-}
+    const rectWidth = 5
+    const rectHeight = 10
+    const clockLine = (i, m) => {
+        ctx.save()
+        ctx.fillStyle = `hsl(${Math.floor(i / m * 360)},100%,50%)`
+        ctx.translate(-rectWidth / 2, -rectHeight / 2)
+        ctx.fillRect(0, 0, rectWidth, rectHeight)
+        ctx.restore()
+    }
 
-const drawRect = () => {
-    const c = new Path2D()
-    c.rect(0, 0, figWidth, figHeight)
-    ctx.stroke(c)
-    ctx.fill(c)
-}
+    ctx.translate(width / 2, height / 2) // move origin to center
+    const step = 60
+    for (let d = 0; d < step; ++d) {
+        ctx.save()
+        const rad = 2 * Math.PI / step * d
+        ctx.rotate(rad)
+        ctx.translate(0, -height / 2 + circleSpacing)
+        clockLine(d, step)
+        ctx.restore()
+    }
 
-const nbFig = 10
-for (let i = 0; i < nbFig; ++i) {
-    ctx.save()
+    const figWidth = 15
+    const figHeight = 15
 
-    // We should really try to use a low descrepency noise for this
+    const drawCircle = () => {
+        const c = new Path2D()
+        c.arc(0, 0, figWidth / 2, 0, 2 * Math.PI)
+        ctx.stroke(c)
+        ctx.fill(c)
+    }
 
-    const rot1 = 2 * Math.PI * Math.random()
-    const rot2 = 2 * Math.PI * Math.random()
-    const radius = (width / 2 - figWidth - circleSpacing) * Math.random()
-    const tx = radius * Math.cos(rot1)
-    const ty = radius * Math.sin(rot1)
+    const drawRect = () => {
+        const c = new Path2D()
+        c.rect(0, 0, figWidth, figHeight)
+        ctx.stroke(c)
+        ctx.fill(c)
+    }
 
-    ctx.translate(tx, ty)
-    ctx.rotate(rot2)
+    const nbFig = 10
+    for (let i = 0; i < nbFig; ++i) {
+        ctx.save()
 
-    ctx.lineWidth = 4;
-    const colRnd = Math.random()
-    ctx.fillStyle = `hsl(${Math.floor(colRnd * 360)},100%,50%)`
-    ctx.strokeStyle = `hsl(${Math.floor((colRnd * 360 + 180) % 360)}, 100%, 50%)`
+        // We should really try to use a low descrepency noise for this
 
-    const elem = Math.floor(Math.random() * 2)
-    switch (elem) {
-        case 0:
-            drawCircle()
-            break
-        case 1:
-            drawRect()
-            break
+        const rot1 = 2 * Math.PI * Math.random()
+        const rot2 = 2 * Math.PI * Math.random()
+        const radius = (width / 2 - figWidth - circleSpacing) * Math.random()
+        const tx = radius * Math.cos(rot1)
+        const ty = radius * Math.sin(rot1)
+
+        ctx.translate(tx, ty)
+        ctx.rotate(rot2)
+
+        ctx.lineWidth = 4;
+        const colRnd = Math.random()
+        ctx.fillStyle = `hsl(${Math.floor(colRnd * 360)},100%,50%)`
+        ctx.strokeStyle = `hsl(${Math.floor((colRnd * 360 + 180) % 360)}, 100%, 50%)`
+
+        const elem = Math.floor(Math.random() * 2)
+        switch (elem) {
+            case 0:
+                drawCircle()
+                break
+            case 1:
+                drawRect()
+                break
+        }
+
+        ctx.restore()
     }
 
     ctx.restore()
 }
+
+drawPattern('black');
+
+const bgRadios = document.querySelectorAll('input[type=radio][name="bgcol"]');
+
+bgRadios.forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+        drawPattern(event.target.value);
+    })
+})
